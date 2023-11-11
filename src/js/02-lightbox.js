27148167b9eb1b3b9f8d1c20a09f7a7c821dea4a
Oskar Przybysz <117311648+ProgramistaWellness@users.noreply.github.com>
2023-11-11 14:04:11 +0100

    Added gallery creator with task-02 specification

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
+
+const createGallery = () => galleryItems.forEach((item) => {
+    const createdListItem = document.createElement('li');
+    gallery.insertAdjacentElement("beforeend", createdListItem);
+
+    const createdLink = document.createElement('a');
+    createdListItem.insertAdjacentElement('beforeend', createdLink);
+    createdLink.href = item.original;
+    createdLink.classList.add('gallery__item');
+
+    const createdImg = document.createElement('img');
+    createdLink.insertAdjacentElement("beforeend", createdImg);
+    createdImg.src = item.preview;
+    createdImg.alt = item.description;
+    createdImg.classList.add('gallery__image');
+    });
+    createGallery();
+
 
 console.log(galleryItems);
