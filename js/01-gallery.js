b130b4f67ff9eb2dc82e0c27de46f14225ac3b51
Oskar Przybysz <117311648+ProgramistaWellness@users.noreply.github.com>
2023-11-14 21:17:07 +0100

    Formated code with Prettier extension

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,51 +1,48 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-const gallery = document.querySelector('.gallery');
+const gallery = document.querySelector(".gallery");
 
-
-   const createGallery = () => galleryItems.forEach((item) => {
-    const createdListItem = document.createElement('li');
+const createGallery = () =>
+  galleryItems.forEach((item) => {
+    const createdListItem = document.createElement("li");
     gallery.insertAdjacentElement("beforeend", createdListItem);
 
-    const createdDiv = document.createElement('div');
-    createdListItem.insertAdjacentElement('beforeend', createdDiv);
-    createdDiv.classList.add('gallery__item');
+    const createdDiv = document.createElement("div");
+    createdListItem.insertAdjacentElement("beforeend", createdDiv);
+    createdDiv.classList.add("gallery__item");
 
-    const createdLink = document.createElement('a');
-    createdDiv.insertAdjacentElement('beforeend', createdLink);
+    const createdLink = document.createElement("a");
+    createdDiv.insertAdjacentElement("beforeend", createdLink);
     createdLink.href = item.original;
-    createdLink.classList.add('gallery__link');
+    createdLink.classList.add("gallery__link");
 
-    const createdImg = document.createElement('img');
+    const createdImg = document.createElement("img");
     createdLink.insertAdjacentElement("beforeend", createdImg);
     createdImg.src = item.preview;
     createdImg.alt = item.description;
     createdImg.dataset.source = item.original;
-    createdImg.classList.add('gallery__image');
-    });
-    createGallery();
-
+    createdImg.classList.add("gallery__image");
+  });
+createGallery();
 
 const handleClick = (event) => {
-    event.preventDefault();
-    function onEscape(event) {
-        if (event.code !== "Escape") return;
-        instance.close();
-    }
-    const instance = basicLightbox.create(`
+  event.preventDefault();
+  function onEscape(event) {
+    if (event.code !== "Escape") return;
+    instance.close();
+  }
+  const instance = basicLightbox.create(
+    `
     <img src= ${event.target.dataset.source} width="1200">
-    `, 
-    {onShow: () => window.addEventListener("keydown", onEscape),
-     onClose: () => window.removeEventListener("keydown", onEscape),
-        }, )
-        instance.show();
+    `,
+    {
+      onShow: () => window.addEventListener("keydown", onEscape),
+      onClose: () => window.removeEventListener("keydown", onEscape),
     }
-    
+  );
+  instance.show();
+};
 
-gallery.addEventListener('click', handleClick);
+gallery.addEventListener("click", handleClick);
 
-
-
-    
 console.log(galleryItems);
-
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,29 +1,29 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-const gallery = document.querySelector('.gallery');
+const gallery = document.querySelector(".gallery");
 
-const createGallery = () => galleryItems.forEach((item) => {
-    const createdListItem = document.createElement('li');
+const createGallery = () =>
+  galleryItems.forEach((item) => {
+    const createdListItem = document.createElement("li");
     gallery.insertAdjacentElement("beforeend", createdListItem);
 
-    const createdLink = document.createElement('a');
-    createdListItem.insertAdjacentElement('beforeend', createdLink);
+    const createdLink = document.createElement("a");
+    createdListItem.insertAdjacentElement("beforeend", createdLink);
     createdLink.href = item.original;
-    createdLink.classList.add('gallery__item');
+    createdLink.classList.add("gallery__item");
 
-    const createdImg = document.createElement('img');
+    const createdImg = document.createElement("img");
     createdLink.insertAdjacentElement("beforeend", createdImg);
     createdImg.src = item.preview;
     createdImg.alt = item.description;
-    createdImg.classList.add('gallery__image');
-    });
-    createGallery();
+    createdImg.classList.add("gallery__image");
+  });
+createGallery();
 
-    const lightbox = new SimpleLightbox('.gallery a', 
-    { /* options */ 
-    captionsData: "alt",
-    captionDelay: 250,
+const lightbox = new SimpleLightbox(".gallery a", {
+  /* options */ captionsData: "alt",
+  captionDelay: 250,
 });
-    lightbox.on('show.simplelightbox');
+lightbox.on("show.simplelightbox");
 
 console.log(galleryItems);
